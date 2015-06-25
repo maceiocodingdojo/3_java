59158bb6d4237f0a58fa20550778c868ed538458
Rachid Calazans <rachidcalazans@gmail.com>
2015-06-25 20:02:33 -0300

    ˜Bruno˜

--- tests/AtmTest.java
+++ tests/AtmTest.java
@@ -1,21 +1,23 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class AtmTest {
+	Atm atm;
+	
+	@Before
+	public void setup(){
+	 atm = new Atm();
+	}
 
-	@Test
-	public void errorWhenWithdrawZero() {
-		Atm atm = new Atm();
-		String actual 	= atm.withdraw(0.0);
-		String expected = "Invalid amount";		
-		assertEquals(expected, actual);
+	@Test(expected = InvalidAmountException.class)
+	public void errorWhenWithdrawZero() throws InvalidAmountException {
+		atm.withdraw(0.0);
 	}
 	
 	@Test(expected = InvalidAmountException.class)
-	public void erroWhenWithdrawByInvalidCell(){
-		Atm atm = new Atm();
+	public void errorWhenWithdrawByInvalidCell() throws InvalidAmountException{
 		atm.withdraw(123.0);
-		
 	}
 }
